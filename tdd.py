47c7f8408217dc49a623fcb15fd0b221f3412a82
miguel0 <miguelbarroe@gmail.com>
2020-11-15 20:40:50 -0700

    initial commit

---
+++ tdd.py
@@ -0,0 +1,29 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+
+class List:
+	def __init__(self, *values):
+		self.head = None
+	
+	def size(self):
+		return None
+	
+	def clear(self):
+		return None
+	
+	def add(self, *values):
+		return None
+	
+	def exists(self, value):
+		return None
+	
+	def getAt(self, index):
+		return None
+	
+	def get(self, value):
+		return None
+	
+	def remove(self, index):
+		return None
