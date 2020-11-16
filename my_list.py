5f528b5a8da3d1841c6c73fae9c8af43be0aced7
miguel0 <miguelbarroe@gmail.com>
2020-11-15 21:46:04 -0700

    add function

--- my_list.py
+++ my_list.py
@@ -33,7 +33,23 @@ class MyList:
 		self.head = None
 	
 	def add(self, *values):
-		return None
+		if len(values) == 0:
+			return
+		
+		if self.head is None:
+			self.head = MyNode(values[0])
+			values = values[1:]
+		
+		if len(values) == 0:
+			return
+		
+		current = self.head
+		while current.next is not None:
+			current = current.next
+		for value in values:
+			temp = MyNode(value)
+			current.next = temp
+			current = temp
 	
 	def exists(self, value):
 		return None
